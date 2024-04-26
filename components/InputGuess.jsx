8a29a254936d343b74f0c75cb3d587c665c38d32
Kam Cheng <kam.cheng03@gmail.com>
2024-04-26 16:14:34 +0100

    using target.value[0] to get value of submitted form input

--- components/InputGuess.jsx
+++ components/InputGuess.jsx
@@ -10,19 +10,31 @@ function InputGuess() {
     const [guess, setGuess] = useState("");
     const handleSubmit = (event) => {
         event.preventDefault();
-        setGuess("new guess");
-        console.log(event.target.value)
-        // return event.target.value
+        // setGuess("new guess");
+        console.log(event.target[0].value)
+        return event.target[0].value
         // send guess to countGuess and incorrectGuesses
     }
 
+    // const handleSubmit = (event) => {
+    //     // prevent the form's default submission behaviour
+    //     event.preventDefault();
+    //     // add the newItem to our list in App
+    //     setList((currList) => {
+    //       return [newItem, ...currList];
+    //     });
+    //     // reset the input to be empty
+    //     setNewItem('');
+    //   };
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="guess"></label>
-            <input id="guess" placeholder="put your guess here"></input>            
-            <button onClick={handleSubmit}>Submit guess</button>
+            <input id="guess" placeholder="put your guess here" ></input>            
+            <button type="submit">Submit guess</button>
         </form>
     )
-    }
+    };
+
     
     export default InputGuess
