24f0533e173abbd3773c0ff947e42251cc1d7a81
Kam Cheng <kam.cheng03@gmail.com>
2024-04-26 15:29:36 +0100

    try to inputGuess

--- components/CountGuess.jsx
+++ components/CountGuess.jsx
@@ -1,5 +1,26 @@
+// function CountGuess() {
+//     return <>countdown of guesses left</>
+// }
+
+// export default CountGuess
+
+import { useState } from "react"
+import InputGuess from "./InputGuess";
+
 function CountGuess() {
-    return <>countdown of guesses left</>
+    const [guessesLeft, setGuessesLeft] = useState(11);
+    const handleClick = () => {
+        setGuessesLeft(currentGuessesLeft => {
+            return currentGuessesLeft - 1
+        })
+    } 
+
+    return (
+    <div>
+        <p>Number of guesses left: {guessesLeft}</p>
+        <button onClick={handleClick}>Decrease guesses</button>
+    </div>
+    )
 }
 
 export default CountGuess
--- components/InputGuess.jsx
+++ components/InputGuess.jsx
@@ -1,5 +1,28 @@
-function InputGuess() {
-    return <>user input form for guess</>
-}
+// function InputGuess() {
+//     return <>user input form for guess</>
+// }
 
-export default InputGuess
+// export default InputGuess
+
+import { useState } from "react";
+
+function InputGuess() {
+    const [guess, setGuess] = useState("");
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setGuess("new guess");
+        console.log(event.target.value)
+        // return event.target.value
+        // send guess to countGuess and incorrectGuesses
+    }
+
+    return (
+        <form>
+            <label htmlFor="guess"></label>
+            <input id="guess" placeholder="put your guess here"></input>            
+            <button onClick={handleSubmit}>Submit guess</button>
+        </form>
+    )
+    }
+    
+    export default InputGuess
